d74015cbdf47f30fb5f84511f8109dd7fed10a73
Shane Krueger <shane@acdmail.com>
2020-09-21 11:59:25 -0400

    Add looksfor parameter (#3)

--- main/main.go
+++ main/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	"io/ioutil"
 )
 
 func main() {
@@ -16,13 +17,19 @@ func main() {
 	var interval = flag.Int("interval", 200, "Interval between polling in ms")
 	var localhost = flag.String("localhost", "", "Ip address to use for localhost")
 	var sleep = flag.Int("sleep", 0, "Duration to sleep prior to request in ms")
+	var lookfor = flag.String("lookfor", "", "String to look for")
 	flag.Parse()
 
-	fmt.Printf("Version 1.2.0\n")
-	fmt.Printf("Sleeping for %d ms\n", *sleep)
-	sleepDuration2 := time.Duration(*sleep) * time.Millisecond
-	time.Sleep(sleepDuration2)
+	fmt.Printf("Version 1.3.0\n")
+	if *sleep > 0 {
+		fmt.Printf("Sleeping for %d ms\n", *sleep)
+		sleepDuration2 := time.Duration(*sleep) * time.Millisecond
+		time.Sleep(sleepDuration2)
+	}
 	fmt.Printf("Polling URL `%s` for response code %d for up to %d ms at %d ms intervals\n", *url, *responseCode, *timeout, *interval)
+	if *lookfor!="" {
+		fmt.Printf("Looking for `%s`\n", *lookfor)
+	}
 	startTime := time.Now()
 	timeoutDuration := time.Duration(*timeout) * time.Millisecond
 	sleepDuration := time.Duration(*interval) * time.Millisecond
@@ -33,8 +40,16 @@ func main() {
 	for {
 		res, err := http.Get(*url)
 		if err == nil && res.StatusCode == *responseCode {
-			fmt.Printf("Response header: %v", res)
-			os.Exit(0)
+			defer res.Body.Close()
+			body, err := ioutil.ReadAll(res.Body)
+			if (err == nil) {
+				bodyString := string(body)
+				if *lookfor=="" || *lookfor==bodyString {
+					fmt.Printf("Response header: %v\n", res)
+					fmt.Printf("Response body: %s\n", bodyString)
+					os.Exit(0)
+				}
+			}
 		}
 		time.Sleep(sleepDuration)
 		elapsed := time.Now().Sub(startTime)
